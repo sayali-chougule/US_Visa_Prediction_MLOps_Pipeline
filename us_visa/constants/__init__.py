78e2c95da26c5f3ee0ce3645653dc56fbe39c3dd
sayali-chougule <sayalichougule2269@gmail.com>
2026-04-29 12:45:34 -0400

    files modified

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -8,6 +8,23 @@ COLLECTION_NAME = "us_visa"
 MONGODB_URL_KEY = "MONGODB_URL"
 
 PIPELINE_NAME: str = "usvisa"
+
 ARTIFACT_DIR: str = "artifact"
 
+FILE_NAME = "EasyVisa.csv.zip"
+
+TRAIN_FILE_NAME = "train.csv"
+
+TEST_FILE_NAME = "test.csv"
+
 MODEL_FILE_NAME = "model.pkl"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
